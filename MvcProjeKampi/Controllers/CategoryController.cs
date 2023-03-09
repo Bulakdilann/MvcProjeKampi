0a3a4cb6d07e463ce1f079f395cc8e1f1791e7d8
heavenssx <119794002+Bulakdilann@users.noreply.github.com>
2023-03-09 14:59:30 +0300

    Mvc Proje Kampı EntityFrameworkDal

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -11,14 +12,14 @@ namespace MvcProjeKampi.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
-        CategoryManager cm = new CategoryManager();
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult GetCategoryList()
         {
-            var categoryvalues = cm.GetAllBL();
+            var categoryvalues = cm.GetList();
             return View(categoryvalues);
         
         }
@@ -30,7 +31,7 @@ namespace MvcProjeKampi.Controllers
         [HttpPost]
         public ActionResult AddCategory(Category p)
         {
-            cm.CategoryAddBL(p);
+           // cm.CategoryAddBL(p);
             return RedirectToAction("GetCategoryList");
         }
     }
