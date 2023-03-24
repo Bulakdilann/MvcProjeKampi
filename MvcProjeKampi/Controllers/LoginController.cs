feff7d4522a565e7f81128e236cc532305916d05
heavenssx <119794002+Bulakdilann@users.noreply.github.com>
2023-03-24 16:21:51 +0300

    Mvc Project Camp / Authentication and Session

---
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class LoginController : Controller
+    {
+        //AdminManager adm = new AdminManager(new EfAdminDal());
+        Context _db = new Context();
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin p)
+        {
+            var adminuserinfo = _db.Admins.FirstOrDefault(x => x.AdminUserName == p.AdminUserName && x.AdminPassword == p.AdminPassword);
+            if (adminuserinfo !=null)
+            {
+                FormsAuthentication.SetAuthCookie(adminuserinfo.AdminUserName,false);
+                Session["AdminUserName"] = adminuserinfo.AdminUserName;
+                return RedirectToAction("Index","AdminCategory");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
